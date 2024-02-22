e3131e6bfbe444526ec0c7797e8c74a273ec469e
fufic123 <semenovmark07@gmail.com>
2024-02-22 19:05:10 +0200

    sss

--- app/settings.py
+++ app/settings.py
@@ -141,10 +141,10 @@ USE_TZ = True
 
 
 STATIC_URL = 'collected_static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "collected_static",
-# ]
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATICFILES_DIRS = [
+    BASE_DIR / "collected_static",
+]
+# STATIC_ROOT = BASE_DIR / 'collected_static'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
 # Default primary key field type
