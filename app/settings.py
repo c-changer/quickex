57bef1d05a5ded8c9fedd013b49152b23363db57
fufic123 <semenovmark07@gmail.com>
2024-02-22 19:08:11 +0200

    sss

--- app/settings.py
+++ app/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATIC_URL = 'collected_static/'
 STATICFILES_DIRS = [
     BASE_DIR / "collected_static",
 ]
