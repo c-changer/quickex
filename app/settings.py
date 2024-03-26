f25e0038d3a40d787cd3811c05f5463dfd0f3e87
Mark Semenov <98762887+fufic123@users.noreply.github.com>
2024-03-26 21:07:17 +0100

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -168,13 +168,13 @@ JAZZMIN_SETTINGS = {
     "site_brand": "qckx.org",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "images/favicon.ico",
+    "site_logo": "images/logo.jpeg",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": "images/favicon.ico",
+    "login_logo": "images/logo.jpeg",
 
     # Logo to use for login form in dark themes (defaults to login_logo)
-    "login_logo_dark": "images/favicon.ico",
+    "login_logo_dark": "images/logo.jpeg",
 
     # CSS classes that are applied to the logo above
     "site_logo_classes": "img-circle",
