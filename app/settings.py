3db3808f0bebc63a8f55367f609075717ccf8d2f
Mark Semenov <98762887+fufic123@users.noreply.github.com>
2024-10-31 19:37:33 +0200

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.bizssages',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     
