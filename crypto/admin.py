fa6f81b22bfff0f095e2bd54cbf225db82202fd8
Mark Semenov <98762887+fufic123@users.noreply.github.com>
2024-11-15 22:43:38 +0200

    Update admin.py

--- crypto/admin.py
+++ crypto/admin.py
@@ -42,6 +42,6 @@ class TGbotAdmin(admin.ModelAdmin):
     list_display = ['name', 'token', 'chat_id']
     def has_add_permission(self, request):
         return False
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
+    def has_delete_permission(self, request, obj=None):
+        return False
 admin.site.register(TGbot, TGbotAdmin)
